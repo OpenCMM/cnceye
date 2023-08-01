fcd52b4bed9beec4e6cf48bd68f0ca26ef8598af
Yuichiro Aoki <45054071+yuichiroaoki@users.noreply.github.com>
2023-08-01 15:45:52 +0900

    Line (#1)
    * test images from blender
    
    * add line
    
    * fmt

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,11 +1,11 @@
 from cnceye import circle
-from cnceye import camera
+from cnceye.camera import calib
 import cv2
 
 
 def test_get_circle():
     image = cv2.imread("tests/fixtures/images/coins.jpg")
-    undistorted_image = camera.undistort_img(
+    undistorted_image = calib.undistort_img(
         image, "tests/fixtures/camera/google-pixel5-5g.json"
     )
 
